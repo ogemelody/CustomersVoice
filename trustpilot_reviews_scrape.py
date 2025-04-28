c6ec92c212baa9765ab33d75bdbdef76763a0465
Melody Egwuchukwu <melodyegwuchukwu1@gmail.com>
2025-04-28 22:18:02 +0200

    get each review,rating, and date from each reviewer

--- trustpilot_reviews_scrape.py
+++ trustpilot_reviews_scrape.py
@@ -1,47 +1,28 @@
+# updated version of the extract of review,date, and rating
 from bs4 import BeautifulSoup
 import requests
 
-# Step 1: Define the URL and headers
 url = "https://www.trustpilot.com/review/duolingo.com?page=1"
 headers = {"User-Agent": "Mozilla/5.0"}
 
-# Step 2: Request the page
 response = requests.get(url, headers=headers)
-
-# Step 3: Parse the HTML
 soup = BeautifulSoup(response.text, "html.parser")
-#print(soup)
 
-"""
-#to get the review written 
-review_block = soup.find("p", attrs={"data-service-review-text-typography": True})
-for block in review_block:
-    full_review = block.get_text(separator=" ", strip=True)
-    print(full_review)
-    #print("-"*60)
-"""
+# Get all review texts
+review_blocks = soup.find_all("p", attrs={"data-service-review-text-typography": True})
+reviews = [block.get_text(separator=" ", strip=True) for block in review_blocks]
 
-"""
-#to get the date of review
-date_block = soup.find("p", attrs={"data-service-review-date-of-experience-typography": True})
-#print(date_block)
-for date in date_block:
-    get_date = date.get_text(separator=" ", strip=True)
-    print(get_date)
-    #print("-"*60)
-"""
+# Get all dates
+date_blocks = soup.find_all("p", attrs={"data-service-review-date-of-experience-typography": True})
+dates = [block.get_text(separator=" ", strip=True) for block in date_blocks]
 
-"""#get the rating
-rating_block = soup.find("div", attrs={"data-service-review-rating": True})
-print(rating_block)
-for rating in rating_block:
-    rating_value = rating["data-service-review-rating"]  # <--- Get attribute like a dictionary
-    print(rating_value)"""
+# Get all ratings
+rating_blocks = soup.find_all("div", attrs={"data-service-review-rating": True})
+ratings = [block["data-service-review-rating"] for block in rating_blocks]
 
-# Find all review headers that have the rating attribute
-review_ratings = soup.find_all("div", attrs={"data-service-review-rating": True})
-
-for header in review_ratings:
-    #print(header)
-    rating = header["data-service-review-rating"]  # <--- Get attribute like a dictionary
-    print(rating)
+# Now print all together
+for i in range(min(len(reviews), len(dates), len(ratings))):
+    print(f"Review {i+1}: {reviews[i]}")
+    print(f"Date: {dates[i]}")
+    print(f"Rating: {ratings[i]}")
+    print("-" * 80)
